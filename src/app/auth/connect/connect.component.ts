63badbb3429fc17826fbbfac86a3661810239687
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-02-10 14:01:46 +0530

    check if agent online

--- src/app/auth/connect/connect.component.ts
+++ src/app/auth/connect/connect.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerConnection } from 'jema';
 import { BackendService } from 'src/app/_shared/backend.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-connect',
@@ -8,17 +10,50 @@ import { BackendService } from 'src/app/_shared/backend.service';
 })
 export class ConnectComponent implements OnInit {
 
+  server: ServerConnection;
+
   constructor(private service: BackendService) { }
 
   ngOnInit() {
     this.service.setupServerConnection();
+    this.server = this.service.getServerConnection();
 
-    this.service.getServerConnection().connectionState.subscribe((connectionState) => {
-      if (connectionState.connected === true)
-        this.service.setAppState({ state: 'Connected' });
-    });
+    this.tryConnect();
+  }
 
-    this.service.connect();
+  tryConnect() {
+
+    this.server.IsOnline().subscribe(
+      () => {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Agent already connected!',
+          text: 'It seems you have another active session. Please disconnect, if you like to login here.',
+          showDenyButton: true,
+          denyButtonText: `Remote Logout`,
+        }).then((result) => {
+          if (result.isDenied) {
+            this.server.RemoteLogout().subscribe(
+              (data) => {
+                Swal.fire({ icon: 'success', title: 'Remote Logout initiated successfully.' });
+              },
+              (err) => {
+                Swal.fire({ icon: 'error', title: 'Oops...', text: err.message });
+              }
+            );
+          }
+        });
+      },
+      (err) => {
+        // console.log(err);
+        this.server.connectionState.subscribe((connectionState) => {
+          if (connectionState.connected === true)
+            this.service.setAppState({ state: 'Connected' });
+        });
+
+        this.service.connect();
+      }
+    );
 
   }
 
