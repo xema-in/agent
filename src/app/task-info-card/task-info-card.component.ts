b6c3c9e6370d0b00bef2361af0a22d1c24b6d80e
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-02-20 13:48:23 +0530

    task info card

--- src/app/task-info-card/task-info-card.component.ts
+++ src/app/task-info-card/task-info-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerConnection } from 'jema';
+import { BackendService } from '../_shared/backend.service';
 
 @Component({
   selector: 'app-task-info-card',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-info-card.component.css']
 })
 export class TaskInfoCardComponent implements OnInit {
+  bus: ServerConnection;
+  task: any;
+  color: string;
+  completed: any;
+  ahtTarget: number;
+  currentprogress: any;
+  interval;
 
-  constructor() { }
+  constructor(private service: BackendService) {
+    this.bus = service.getServerConnection();
+  }
 
   ngOnInit(): void {
+
+    this.bus.task.subscribe((task) => {
+
+      this.task = task;
+
+      if (this.task && this.task.call.attributes.destconnectedlinenum) {
+        if (this.task.call.attributes.destconnectedlinenum.startsWith('+91')) {
+          this.task.call.attributes.destconnectedlinenum = this.task.call.attributes.destconnectedlinenum.substring(
+            3
+          );
+        }
+        clearInterval(this.interval);
+        if (this.task.queue.baseQueueOptions.ahtTarget) {
+          this.ahtTarget = this.task.queue.baseQueueOptions.ahtTarget;
+          this.currentprogress = 0;
+          this.startTimer();
+        }
+      }
+
+    });
+
+  }
+
+  GetDuration(seconds) {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor((seconds % 3600) % 60);
+    const hDisplay = h > 0 ? h + (h === 1 ? ' h ' : ' h ') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? ' min ' : ' min ') : '';
+    const sDisplay = s > 0 ? s + (s === 1 ? ' s' : ' s') : '';
+    return hDisplay + mDisplay + sDisplay;
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.currentprogress += 1;
+      this.completed = (this.currentprogress / this.ahtTarget) * 100;
+      this.color = 'green';
+      if (this.currentprogress > this.ahtTarget) {
+        this.color = 'red';
+        // clearInterval(this.interval);
+      }
+    }, 1000);
   }
 
 }
